a062184f0a8598a7bf0cd8435e3cae69f4975e01
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-02-21 23:24:43 +0100

    bugs solved

--- src/quiz.js
+++ src/quiz.js
@@ -22,7 +22,7 @@ class Quiz {
         const questions = this.questions.slice();
         for (let i = questions.length - 1; i > 0; i--) {
             const randomIndex = Math.floor(Math.random() * i);
-            [questions[i], questions[randomIndex]] = [questions[randomIndex], questions[i]];
+           
         }
         this.questions = questions;
     }
