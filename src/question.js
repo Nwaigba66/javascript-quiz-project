aaa72c81eeb76bc60c05f57413bc0cd9fc93ac8d
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-02-22 08:56:59 +0100

    jasmine

--- src/question.js
+++ src/question.js
@@ -10,7 +10,7 @@ class Question {
         const choices = this.choices.slice();
         for (let i = choices.length - 1; i > 0; i--) {
             const randomIndex = Math.floor(Math.random() * i);
-            
+            [choices[i], choices[randomIndex]] = [choices[randomIndex], choices[i]];
         }
         this.choices = choices;
     }
